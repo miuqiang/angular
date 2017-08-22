e6da9c5612d15a9ebeddf96f4e391498a50f5410
miuqiang <763436128@qq.com>
2017-08-22 14:40:04 +0800

    新增了评价组件，以及修改了些样式

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+	selector: 'app-product',
+	templateUrl: './product.component.html',
+	styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+	private products: Array<Product>; //存储商品数据
 
-  ngOnInit() {
-  }
+	constructor() { }
+		// 初始化数组
+		ngOnInit() {
+			this.products = [
+				new Product(1,"第一个商品",9.90,2,"这是第一个商品，我是第一个商品",["电子产品","苹果"]),
+				new Product(2,"第二个商品",9.90,4,"这是第二个商品，我是第二个商品",["书记","儿童"]),
+				new Product(3,"第三个商品",9.90,5,"这是第三个商品，我是第三个商品",["电子产品","苹果"]),
+				new Product(4,"第四个商品",9.90,1,"这是第四个商品，我是第四个商品",["电子产品","苹果"]),
+				new Product(5,"第五个商品",9.90,5,"这是第五个商品，我是第五个商品",["水果","苹果"]),
+				new Product(6,"第六个商品",9.90,3,"这是第六个商品，我是第六个商品",["电子产品","苹果"]),
+				new Product(7,"第七个商品",1.90,2,"这是第六个商品，我是第六个商品",["电子产品","苹果"])
+			];
+	}
+}
 
+// 定义一个产品类
+export class Product{
+	constructor(
+		public id:number,
+		public title:string,
+		public price:number,
+		public rating:number,
+		public desc:string,
+		public categories:Array<string>
+	){
+
+	}
 }
--- src/app/starts/starts.component.ts
+++ src/app/starts/starts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-starts',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartsComponent implements OnInit {
 
+  private starts: boolean[];
+
+  @Input()
+  private rating: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+    this.starts = [];
+    for(let i=1;i<=5;i++){
+      this.starts.push(i>this.rating);
+    }
   }
 
 }
